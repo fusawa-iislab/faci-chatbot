47d15762ae91f98dfac6149d70a214e43de94a79
fusawayugo <fusawayugo0712@g.ecc.u-tokyo.ac.jp>
2024-11-26 17:43:07 +0900

    a

--- frontend/src/components/ParticipantBot/index.tsx
+++ frontend/src/components/ParticipantBot/index.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import Silhoutte from '../../assets/images/person- silhouette.svg';
 import styles from './styles.module.css';
 
+import {Participant} from "../../assets/structs";
 
-export type ParticipantBotProps = {
-    name?: string;
-    persona?: string;
+
+
+export type ParticipantBotProps = Participant & {
+    comment?: string|null; 
 }
 
 const ParticipantBot : React.FC<ParticipantBotProps> = ({
     name = "participant",
     persona = "",
+    background = "",
+    comment = null,
 }) =>{
 
     return (
         <div className={styles["participant-wrapper"]}>
-            <div className={styles["comment-wrapper"]}></div>
+            {comment && <p className={styles["comment"]}>{comment}</p>}
             <img src={Silhoutte} className={styles["siloutte-image"]}/>
             <p className={styles["participant-name"]}>{name}</p>
         </div>
